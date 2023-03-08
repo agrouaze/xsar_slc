f3c29070fd89d0a4767f723fcfd36eb83eabe15f
grouazel <antoine.grouazel@ifremer.fr>
2023-03-08 15:01:19 +0100

    improve WV script L1B with latest config file mangement

--- scripts/L1B_xspectra_WV_SLC_IFR.py
+++ scripts/L1B_xspectra_WV_SLC_IFR.py
@@ -18,8 +18,9 @@ import logging
 import os
 import time
 import xsarslc
-from xsarslc.get_config_infos import get_IR_file
-PRODUCT_VERSION = '0.10' # see https://github.com/umr-lops/xsar_slc/wiki/processings
+from xsarslc.get_config_infos import get_IR_file, get_production_version, get_default_outputdir, get_default_xspec_params, \
+    get_default_landmask_dir
+PRODUCT_VERSION = get_production_version()  # see  https://github.com/umr-lops/xsar_slc/wiki/IFR--IW-processings
 def get_memory_usage():
     try:
         import resource
@@ -30,36 +31,49 @@ def get_memory_usage():
     str_mem = 'RAM usage: %1.1f Go'%memory_used_go
     return str_mem
 
-def generate_WV_L1Bxspec_product(slc_wv_path,output_filename, polarization=None,dev=False):
+def generate_WV_L1Bxspec_product(slc_wv_path,output_filename, xspeconfigname,polarization=None,dev=False,landmask=None):
     """
 
     :param tiff: str full path
     :param output_filename : str full path
+    :param xspeconfigname : str (eg 'tiles20km')
     :param polarization : str : VV VH HH HV [optional]
     :apram dev: bool: allow to shorten the processing
+    :param landmask : landmask obj (eg : cartopy.feature.NaturalEarthFeature() )
     :return:
     """
     safe = os.path.dirname(os.path.dirname(slc_wv_path))
     logging.info('start loading the datatree %s', get_memory_usage())
+
+    xspec_params = get_default_xspec_params(config_name=xspeconfigname)
+    tile_width_intra = xspec_params['tile_width_intra']
+    tile_overlap_intra = xspec_params['tile_overlap_intra']
+    periodo_width_intra = xspec_params['periodo_width_intra']
+    periodo_overlap_intra = xspec_params['periodo_overlap_intra']
+    logging.info('tile_width_intra : %s', tile_width_intra)
+
     imagette_number = os.path.basename(slc_wv_path).split('-')[-1].replace('.tiff', '')
     str_gdal = 'SENTINEL1_DS:%s:WV_%s' % (safe, imagette_number)
-    bu = xsar.Sentinel1Meta(str_gdal)._bursts
     chunksize = {'line': 6000, 'sample': 7000}
     xsarobj = xsar.Sentinel1Dataset(str_gdal, chunks=chunksize)
     dt = xsarobj.datatree
     dt.load() #took ?min to load and ? Go RAM
     logging.info('datatree loaded %s',get_memory_usage())
     unit = safe[0:3]
-    subswath = str_gdal.split(':')[2]
     subswath = dt['image'].ds['swath_subswath'].values
-    IR_dir = '/home/datawork-cersat-public/project/sarwave/data/products/developments/aux_files/sar/impulse_response/'
-    IR_path = get_IR_file(unit, subswath, polarization.upper(), auxdir=IR_dir)
+    IR_path = get_IR_file(unit, subswath, polarization.upper())
     xs0 = proc.compute_WV_intraburst_xspectra(dt=dt,
-                                         polarization=polarization, periodo_width={"line": 2000, "sample": 2000},
-                                         periodo_overlap={"line": 1000, "sample": 1000},IR_path=IR_path)
-    xs = xs0.swap_dims({'freq_line': 'k_az', 'freq_sample': 'k_rg'})
-    xs = xspectra.symmetrize_xspectrum(xs, dim_range='k_rg', dim_azimuth='k_az')
-    xs = netcdf_compliant(xs) # to split complex128 variables into real and imag part
+                                         polarization=polarization,
+                                              tile_width_intra=tile_width_intra,
+                                              tile_overlap_intra=tile_overlap_intra,
+                                              periodo_width_intra=periodo_width_intra,
+                                              periodo_overlap_intra=periodo_overlap_intra,
+                                         #      periodo_width={"line": 2000, "sample": 2000},
+                                         # periodo_overlap={"line": 1000, "sample": 1000},
+                                              IR_path=IR_path)
+    #xs = xs0.swap_dims({'freq_line': 'k_az', 'freq_sample': 'k_rg'})
+    #xs = xspectra.symmetrize_xspectrum(xs, dim_range='k_rg', dim_azimuth='k_az')
+    xs = netcdf_compliant(xs0) # to split complex128 variables into real and imag part
     xs = xs.drop('pol')
     if 'spatial_ref' in xs:
         xs = xs.drop('spatial_ref')
@@ -91,8 +105,17 @@ if __name__ == '__main__':
     parser.add_argument('--overwrite', action='store_true', default=False,
                         help='overwrite the existing outputs [default=False]', required=False)
     parser.add_argument('--tiff', required=True, help='tiff file full path WV SLC')
-    parser.add_argument('--outputdir', required=True, help='directory where to store output netCDF files')
+    parser.add_argument('--outputdir', required=False, help='directory where to store output netCDF files',
+                        default=get_default_outputdir(mode='wv'))
+    parser.add_argument('--version',
+                        help='set the output product version (e.g. 1.4) default version will be read from config.yml',
+                        required=False, default=PRODUCT_VERSION)
     parser.add_argument('--dev', action='store_true', default=False,help='dev mode stops the computation early')
+    parser.add_argument('--landmask', required=False, default=get_default_landmask_dir(),
+                        help='landmask files (such as cartopy /.local/share/cartopy ) to have a landmask information '
+                             'without web connexion , default value cromes from config.yml')
+    parser.add_argument('--xspeconfigname', required=False, default='tiles20km',
+                        help='name of the cross-spectra (tiles/periodogram) in config.yml e.g. "tiles20km" ')
     args = parser.parse_args()
     fmt = '%(asctime)s %(levelname)s %(filename)s(%(lineno)d) %(message)s'
     if args.verbose:
@@ -104,18 +127,31 @@ if __name__ == '__main__':
     t0 = time.time()
 
     slc_wv_path = args.tiff
+    logging.info('product version to produce: %s', args.version)
+    logging.info('outputdir will be: %s', args.outputdir)
+    logging.info('xspeconfigname : %s', args.xspeconfigname)
     subswath_number = os.path.basename(slc_wv_path).split('-')[1]
     polarization_from_file = os.path.basename(slc_wv_path).split('-')[3].upper()
     subsath_nickname = '%s_%s' % (subswath_number, polarization_from_file)
     safe_basename = os.path.basename(os.path.dirname(os.path.dirname(slc_wv_path)))
-    output_filename = os.path.join(args.outputdir,safe_basename, os.path.basename(
+    output_filename = os.path.join(args.outputdir, args.version,safe_basename, os.path.basename(
         slc_wv_path).replace('.tiff','') + '_L1B_xspec_IFR_' + PRODUCT_VERSION + '.nc')
     logging.info('mode dev is %s',args.dev)
+
+    if 'cartopy' in args.landmask:
+        logging.info('landmask is a cartopy feature')
+        import cartopy
+
+        cartopy.config['pre_existing_data_dir'] = args.landmask
+        landmask = cartopy.feature.NaturalEarthFeature('physical', 'land', '10m')
+    else:
+        landmask = None
     logging.info('output filename would be: %s',output_filename)
     if os.path.exists(output_filename) and args.overwrite is False:
         logging.info('%s already exists', output_filename)
     else:
-        generate_WV_L1Bxspec_product(slc_wv_path=slc_wv_path,output_filename=output_filename, dev=args.dev,
-                                     polarization=polarization_from_file)
+        generate_WV_L1Bxspec_product(slc_wv_path=slc_wv_path,output_filename=output_filename,
+                                     xspeconfigname=args.xspeconfigname, dev=args.dev,
+                                     polarization=polarization_from_file, landmask=landmask)
     logging.info('peak memory usage: %s Mbytes', get_memory_usage())
     logging.info('done in %1.3f min', (time.time() - t0) / 60.)
